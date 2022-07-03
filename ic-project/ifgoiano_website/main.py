103f9ddb453dae061799209471da29cc7889a1e9
Fael123Programming <rafaelfonseca1020@gmail.com>
2022-07-03 15:13:51 -0300

    new changes

--- ic-project/ifgoiano_website/main.py
+++ ic-project/ifgoiano_website/main.py
@@ -2,7 +2,10 @@ from scraping_bot import ScrapingBot
 
 
 if __name__ == "__main__":
-    date_string = 'Publicado: Sexta, 25 de Dezembro de 2022, 09h30 '
     sb = ScrapingBot()
-    sb.fetch_all_posts()
-    # print(sb.fetch_featured_post())
+    posts = sb.fetch_all_posts()
+    print('Total of posts:', len(posts))
+    print('-'*100)
+    for post in posts:
+        print(post)
+    print('-'*100)
--- ic-project/ifgoiano_website/scraping_bot.py
+++ ic-project/ifgoiano_website/scraping_bot.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 from webdriver_manager.firefox import GeckoDriverManager
 from post import Post
 from date_converter import DateConverter
@@ -12,6 +15,7 @@ class ScrapingBot:
 
     def __init__(self):
         self._url = 'https://www.ifgoiano.edu.br/home/index.php'
+        self._driver = None
 
     @property
     def url(self) -> str:
@@ -24,47 +28,52 @@ class ScrapingBot:
         self._url = url
 
     def fetch_featured_post(self) -> Post:
-        driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
-        driver.get(self._url)
-        driver.implicitly_wait(0.5)
-        post = Post()
-        featured_post_link = driver.find_element(By.CLASS_NAME, 'manchete-texto-lateral')
+        self._driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+        self._driver.get(self._url)
+        self._driver.implicitly_wait(0.5)
+        featured_post_link = self._driver.find_element(By.CLASS_NAME, 'manchete-texto-lateral')
         featured_post_link.click()  # Click featured post.
-        document_first_heading = driver.find_element(By.CLASS_NAME, 'documentFirstHeading')
+        post = self._get_post()
+        self._driver.quit()
+        return post
+
+    def fetch_all_posts(self) -> list:
+        self._driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+        self._driver.get(self._url)
+        self._driver.implicitly_wait(5)
+        post_list_btn = self._driver.find_element(By.CLASS_NAME, 'link')
+        post_list_btn.click()
+        post_list = WebDriverWait(self._driver, 20).until(ec.presence_of_element_located((By.CLASS_NAME, 'tile-list-1')))
+        post_links = WebDriverWait(post_list, 20).until(ec.presence_of_all_elements_located((By.TAG_NAME, 'a')))
+        links = list()
+        for link in post_links:
+            link_text = link.text
+            if link_text.isspace() or len(link_text) == 0:
+                continue
+            links.append(link_text)
+        posts = list()
+        for i in range(len(links) - 1):  # The last post is rejected
+            self._driver.find_element(By.LINK_TEXT, links[i]).click()
+            posts.append(self._get_post())
+            self._driver.back()
+        self._driver.quit()
+        return posts
+
+    def _get_post(self) -> Post:
+        post = Post()
+        document_first_heading = self._driver.find_element(By.CLASS_NAME, 'documentFirstHeading')
         post_title = document_first_heading.text  # Get post title.
-        document_publish_date = driver.find_element(By.CLASS_NAME, 'documentPublished')
+        document_publish_date = self._driver.find_element(By.CLASS_NAME, 'documentPublished')
         post_publish_date = document_publish_date.text  # Get post publish date text
-        post_accesses = driver.find_element(By.CLASS_NAME, 'documentHits')
-        post_accesses = int(post_accesses.text.split(' ')[1])
-        post_description = driver.find_element(By.CLASS_NAME, 'description')
+        try:
+            post_accesses = int(self._driver.find_element(By.CLASS_NAME, 'documentHits').text.split(' ')[1])
+            # In some posts, there are no information about access measurement.
+        except NoSuchElementException:
+            post_accesses = 0
+        post_description = self._driver.find_element(By.CLASS_NAME, 'description')
         post.title = post_title
         post.publication_date = DateConverter.convert(post_publish_date)
         post.accesses = post_accesses
         post.description = post_description.text.strip()
         PostCategorySetter.set(post)
-        driver.quit()
         return post
-
-    def fetch_all_posts(self) -> list:
-        driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
-        driver.get(self._url)
-        driver.implicitly_wait(0.5)
-        post_list_btn = driver.find_element(By.CLASS_NAME, 'link')
-        post_list_btn.click()
-        driver.implicitly_wait(5)
-        admin_form = driver.find_element(By.ID, 'adminForm')
-        post_list = admin_form.find_element(By.CLASS_NAME, 'tile-list-1')
-        post_links = post_list.find_elements(By.TAG_NAME, 'a')
-        posts = list()
-        for i in range(len(post_links) - 1):  # The last post is rejected
-            post_links[i].click()
-            driver.implicitly_wait(5)
-            # get post info
-            # create post object
-            # posts.append(post)
-            driver.back()
-            driver.implicitly_wait(5)
-            admin_form = driver.find_element(By.ID, 'adminForm')
-            post_list = admin_form.find_element(By.CLASS_NAME, 'tile-list-1')
-            post_links = post_list.find_elements(By.TAG_NAME, 'a')
-        return posts
